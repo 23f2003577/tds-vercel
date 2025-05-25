879b50ceb6a1da29fdd5021f198225f1879e237c
23f2003577 <23f2003577@ds.study.iitm.ac.in>
2025-05-25 15:51:39 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-@app.get("/")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins, adjust as needed
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods, adjust as needed
+    allow_headers=["*"],  # Allows all headers, adjust as needed
+)
+
+@app.get("/")
 def index():
     return {"message": "Hello, World!"}
 
+@app.get("/api")
+def search(request: Request):
+    parameters = list()
+
+    for param in request.query_params.keys():
+        parameters.append(param)
+
+    return {"parameters": parameters}
